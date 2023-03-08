56e2d7459c71a1a79d54254422a3e691da9b33e9
deltastro <deltastrofficial@gmail.com>
2023-03-08 20:50:56 -0300

    added a set-time command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from discord import app_commands
 import discord
 import time
 from discord.ext import commands, tasks
@@ -36,6 +37,15 @@ async def on_ready():
 @bot.tree.command(name='time', description='what time is it?')
 async def timeNow(interaction: discord.Interaction):
     await interaction.response.send_message('It is currently <t:{}:F>.'.format(time_loop.now))
+
+@bot.tree.command(name='set-time', description='set a day for it to be seen as unix timestamp!')
+@app_commands.describe(date_selected='Type here your date! Use YYYY-MM-DD HH:MM:SS format!')
+async def setTime(interaction: discord.Interaction, date_selected: str):
+    try:
+        unix_time = time.mktime(time.strptime(date_selected, '%Y-%m-%d %H:%M:%S'))
+        await interaction.response.send_message('Your date is: <t:{}:F>'.format(int(unix_time)))
+    except Exception as e:
+        await interaction.response.send_message('Sorry, but {} does not match the format!'.format(date_selected), ephemeral=True)
 
 fileToken = open("token.txt", "r")
 token = fileToken.read()
